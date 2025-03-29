0d093991515fc224d7b4f65cd736fade5ef9630b
Ryuukyz <srjoaovictor1@gmail.com>
2025-03-29 19:17:06 -0300

    começo das prioridades do back end

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,8 +1,11 @@
 const db = require('../config/db');
+const axios = require('axios');
+const ML_API_URL = process.env.ML_API_URL || 'http://localhost:5000';
 
-// ryuukyz: criar novo evento
+
+// ryuukyz: criar novo evento || ATT: com prioridade
 const criarEvento = async (req, res) => {
-    const { titulo, conteudo, data_evento, endereco } = req.body;
+    const { titulo, conteudo, data_evento, endereco, duracao = 1, importancia = 3 } = req.body; // ryuukyz: att com prioridade
     const usuario_id = req.usuario_id;
 
     if (!titulo || !conteudo || !data_evento || !endereco) {
@@ -10,12 +13,23 @@ const criarEvento = async (req, res) => {
     }
 
     try {
-        const query = 'INSERT INTO eventos (titulo, conteudo, data_evento, endereco, usuario_id) VALUES (?, ?, ?, ?, ?)';
-        db.query(query, [titulo, conteudo, data_evento, endereco, usuario_id], (err, results) => {
+        // ryuukyz: nao sei ao certo se isso ta certo ou se vai funcionar
+        const { data: { prioridade } } = await axios.post(`${ML_API_URL}/horarios`, {
+            dia_da_semana: new Date(data_evento).getDay(),
+            duracao,
+            importancia
+        });
+        // ryuukyz: aa com campos novos
+        const query = 'INSERT INTO eventos (titulo, conteudo, data_evento, endereco, usuario_id, prioridade, duracao, importancia) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        db.query(query, [titulo, conteudo, data_evento, endereco, usuario_id, prioridade, duracao, importancia], (err, results) => {
             if (err) {
                 return res.status(400).send('Erro ao criar evento.');
             }
-            return res.status(200).json({ id: results.insertId, mensagem: 'Evento criado com sucesso!' });
+            return res.status(200).json({ 
+                id: results.insertId, 
+                prioridade, 
+                mensagem: 'Evento criado com sucesso!' 
+            });
         });
     } catch (error) {
         res.status(500).send('Erro no servidor.');
@@ -60,19 +74,27 @@ const obterEventoPorId = async (req, res) => {
     }
 };
 
-// ryuukyz: atualizar evento
+// ryuukyz: atualizar evento || ATT: com prioridade
 const atualizarEvento = (req, res) => {
     const { id } = req.params;
-    const { titulo, conteudo, data_evento, endereco } = req.body;
+    const { titulo, conteudo, data_evento, endereco, prioridade } = req.body;
     const usuario_id = req.usuario_id;
 
     if (!titulo || !conteudo || !data_evento || !endereco) {
         return res.status(400).send('Título, conteúdo, data do evento e endereço são obrigatórios.');
     }
 
+    if (prioridade && (prioridade < 1 || prioridade > 5)) {
+        return res.status(400).send('Prioridade deve ser entre 1 e 5');
+    }
+
     try {
-        const query = 'UPDATE eventos SET titulo = ?, conteudo = ?, data_evento = ?, endereco = ? WHERE id = ? AND usuario_id = ?';
-        db.query(query, [titulo, conteudo, data_evento, endereco, id, usuario_id], (err, results) => {
+        // ternario aq
+        const query = prioridade ? 'UPDATE eventos SET titulo = ?, conteudo = ?, data_evento = ?, endereco = ?, prioridade = ? WHERE id = ? AND usuario_id = ?' : 'UPDATE eventos SET titulo = ?, conteudo = ?, data_evento = ?, endereco = ? WHERE id = ? AND usuario_id = ?';
+        
+        const values = prioridade ? [titulo, conteudo, data_evento, endereco, prioridade, id, usuario_id] : [titulo, conteudo, data_evento, endereco, id, usuario_id];
+
+        db.query(query, values, (err, results) => {
             if (err) {
                 return res.status(500).send('Erro ao atualizar evento.');
             }
